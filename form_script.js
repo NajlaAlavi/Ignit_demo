36383fb7f5496417f7ed1211046617baa2736104
NajlaAlavi <naaj25alv@gmail.com>
2022-08-28 19:31:33 +0300

    Modified

--- form_script.js
+++ form_script.js
@@ -6,3 +6,25 @@ function validateForm(){
 		return false;
 	}
 }
+
+function CheckPassword(inputtxt){
+	var passw = /^[A-Za-z]\w{7,14}$/, letters = /^[A-Za-z]+$/ ;
+	if (inputtxt.value.match(passw){
+		return true;
+	}
+	else{
+		alert('Password should contain Alphabets, numeric, and special characters ')
+		return false;
+	}
+}
+
+// function allLetter(inputtxt){
+	// var letters = /^[A-Za-z]+$/;
+	// if (inputtxt.value.match(letters){
+		// return true;
+	// }
+	// else{
+		// alert('First Name and Last Name should only contain alphabets.')
+		// return false;
+	// }
+// }
